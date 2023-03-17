7de774a61030193437ea0ac51c59a21987a7d5b1
DUDA <mariabevenuto062@gmail.com>
2023-03-17 15:40:34 -0300

    first init

---
+++ src/pages/HomePage/index.js
@@ -0,0 +1,11 @@
+import { Title } from "./style";
+
+function HomePage() {
+    return (
+      <>
+        <Title>oi, sou a homepage goxtosa</Title>
+      </>
+    );
+  }
+  
+  export default HomePage;
